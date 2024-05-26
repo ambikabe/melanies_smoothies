996b79e972366f5ac2f79abf3e64037e421ea454
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-05-26 13:42:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.write("Choose the fruits you want in your custom smoothie!")
 #st.write("You selected:", option)
 
 #session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
