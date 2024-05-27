314052a3211e1a719ad03ef42771fc871d375fff
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-05-27 13:56:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ st.write("Choose the fruits you want in your custom smoothie!")
 
 #st.write("You selected:", option)
 
-#session = get_active_session()
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
