b29bec294276ff126236af3d0d33b09ce0076f49
ambikabe <160489119+ambikabe@users.noreply.github.com>
2024-05-27 15:55:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,8 @@ session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
+cnx=st.connection("snowflake")
+session=cnx.session()
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 st.write(ingredients_list)
@@ -49,6 +51,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-cnx=st.connection("snowflake")
-session=cnx.session()
+
 
